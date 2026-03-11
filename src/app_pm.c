1ac3c75ed201f54d94d4df28bac3dff32a1b644f
Fedor <lfa1@yandex.ru>
2026-03-11 20:50:15 +0300

    Update app_pm.c

--- src/app_pm.c
+++ src/app_pm.c
@@ -45,6 +45,64 @@ void app_wakeupPinLevelChange() {
     drv_pm_wakeupPinLevelChange(pin_PmCfg, 1);
 }
 
+static void app_drv_pm_lowPowerEnter(void) {
+#if PM_ENABLE
+    drv_pm_wakeup_src_e wakeupSrc = PM_WAKEUP_SRC_PAD;
+    u32 sleepTime = 0;
+    bool longSleep = 0;
+
+#if !defined(__PROJECT_TL_BOOT_LOADER__) && !defined(__PROJECT_TL_SNIFFER__)
+    if (tl_stackBusy() || !zb_isTaskDone()) {
+        return;
+    }
+
+    apsCleanToStopSecondClock();
+#endif
+
+    u32 r = drv_disable_irq();
+
+    ev_timer_event_t *timerEvt = ev_timer_nearestGet();
+    if (timerEvt) {
+        wakeupSrc |= PM_WAKEUP_SRC_TIMER;
+        sleepTime = timerEvt->timeout;
+
+        if (sleepTime) {
+            if (sleepTime > PM_NORMAL_SLEEP_MAX) {
+                longSleep = 1;
+            } else {
+                longSleep = 0;
+            }
+        } else {
+            drv_restore_irq(r);
+            return;
+        }
+    }
+
+    DEBUG(DEBUG_PM_EN, "Not long deep sleep start with time: %d ms\r\n", sleepTime);
+
+#if defined(MCU_CORE_826x)
+    drv_pm_sleep_mode_e sleepMode = (wakeupSrc & PM_WAKEUP_SRC_TIMER) ? PM_SLEEP_MODE_SUSPEND : PM_SLEEP_MODE_DEEPSLEEP;
+#elif defined(MCU_CORE_8258) || defined(MCU_CORE_8278) || defined(MCU_CORE_B91) || defined(MCU_CORE_B92) || defined(MCU_CORE_TL721X) || defined(MCU_CORE_TL321X)
+    drv_pm_sleep_mode_e sleepMode = (wakeupSrc & PM_WAKEUP_SRC_TIMER) ? PM_SLEEP_MODE_DEEP_WITH_RETENTION : PM_SLEEP_MODE_DEEPSLEEP;
+#endif
+
+#if !defined(__PROJECT_TL_BOOT_LOADER__) && !defined(__PROJECT_TL_SNIFFER__)
+    rf_paShutDown();
+    if (sleepMode == PM_SLEEP_MODE_DEEPSLEEP) {
+        drv_pm_deepSleep_frameCnt_set(ss_outgoingFrameCntGet());
+    }
+#endif
+
+    if (!longSleep) {
+        drv_pm_sleep(sleepMode, wakeupSrc, sleepTime);
+    } else {
+        drv_pm_longSleep(sleepMode, wakeupSrc, sleepTime);
+    }
+
+    drv_restore_irq(r);
+#endif
+}
+
 void app_lowPowerEnter() {
 
 //    printf("app_lowPowerEnter(). g_appCtx.not_sleep: %d\r\n", g_appCtx.not_sleep);
@@ -55,7 +113,11 @@ void app_lowPowerEnter() {
 
     if (g_appCtx.not_sleep) {
         /* SDK deep sleep with SRAM retention */
+#if DEBUG_PM_EN
+        app_drv_pm_lowPowerEnter();
+#else
         drv_pm_lowPowerEnter();
+#endif
     } else /*if (zb_isDeviceJoinedNwk())*/{
         /* app deep sleep with SRAM retention */
         if (tl_stackBusy() || !zb_isTaskDone()) {
