d01da7e4fdef5b41ab7338653a4cc7f61b79e8b2
Fedor <lfa1@yandex.ru>
2026-05-05 19:40:26 +0300

    Update app_utility.h

--- src/include/app_utility.h
+++ src/include/app_utility.h
@@ -68,7 +68,7 @@ int32_t poll_rateAppCb(void *arg);
 int32_t delayedMcuResetCb(void *arg);
 int32_t delayedFactoryResetCb(void *arg);
 int32_t delayedFullResetCb(void *arg);
-void app_setPollRate(uint32_t sec, uint8_t poll_rate);
+void app_setPollRate(uint32_t ms, uint8_t poll_rate);
 int32_t net_steer_start_offCb(void *args);
 uint8_t checksum(uint8_t *data, uint16_t length);
 
