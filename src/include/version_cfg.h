5859bbbd7646ad7f5e24f60156437297a7ac8353
Fedor <lfa1@yandex.ru>
2026-07-26 20:32:03 +0300

    new version

--- src/include/version_cfg.h
+++ src/include/version_cfg.h
@@ -28,7 +28,7 @@
 #include "../common/comm_cfg.h"
 
 #define APP_RELEASE                         0x10        //app release 1.0
-#define APP_BUILD                           0x06        //app build 06, full version - v1.0.06
+#define APP_BUILD                           0x07        //app build 07, full version - v1.0.07
 
 #define STACK_RELEASE                       0x30        //stack release 3.0
 #define STACK_BUILD                         0x01        //stack build 01
